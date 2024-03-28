e844bc5120204f55c4c1cc54c177b36522d34401
jamaspy <aspin@me.com>
2024-03-28 14:22:44 +1100

    mobile nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,28 @@
+'use client';
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Menu } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
 const Header = () => {
+  const [state, setState] = React.useState(false);
+  const router = useRouter();
+
+  const handleNavigate = (path: string) => {
+    router.push(path);
+    setState(false);
+  };
   return (
     <nav className="sticky top-0 w-full grid grid-cols-3 p-2 bg-george-orange">
-      <div className="flex-1 col-span-1">
+      <Link href="/" className="flex-1 col-span-1">
         <Image
           src="/Logo_Orange_InvertedWhite.svg"
           alt="georgies house"
@@ -13,7 +30,7 @@ const Header = () => {
           height={30}
           className=""
         />
-      </div>
+      </Link>
       <div className="z-30 hidden md:flex flex-row items-end justify-start gap-12 flex-1 col-span-2 font-katarine text-xl text-white">
         <Link
           className="hover:text-george-lilac font-semibold transition-all duration-200 ease-in"
@@ -41,7 +58,32 @@ const Header = () => {
         </Link>
       </div>
       <div className="flex md:hidden ml-auto my-auto col-span-2">
-        <Menu size={32} className="text-white hover:text-george-lilac" />
+        <DropdownMenu open={state} onOpenChange={setState}>
+          <DropdownMenuTrigger data-state={state}>
+            <Menu size={32} className="text-white hover:text-george-lilac" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mr-2 px-12 py-4">
+            <DropdownMenuLabel
+              className="text-2xl font-katarine"
+              onClick={() => handleNavigate('/')}
+            >
+              Home
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="text-2xl font-katarine"
+              onClick={() => handleNavigate('/team')}
+            >
+              Our Team
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="text-2xl font-katarine"
+              onClick={() => handleNavigate('/contact')}
+            >
+              Contact Us
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
